6c741614637e1403b5ab9246c505abf27711742d
visakh427799 <60034460+visakh427799@users.noreply.github.com>
2020-07-20 17:55:31 +0530

    Updates

---
+++ precache-manifest.cad618f403b8099aa02f525ac6f29558.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8d8a1ebb387d6fa636d3ca3f5871ba",
+    "url": "/React-projects/index.html"
+  },
+  {
+    "revision": "86766d7444eca273e85a",
+    "url": "/React-projects/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f791b1a72d896a969d8b",
+    "url": "/React-projects/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86766d7444eca273e85a",
+    "url": "/React-projects/static/js/2.c88b2228.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-projects/static/js/2.c88b2228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f791b1a72d896a969d8b",
+    "url": "/React-projects/static/js/main.6c650bc3.chunk.js"
+  },
+  {
+    "revision": "3f1ccadc90070a16fa1e",
+    "url": "/React-projects/static/js/runtime-main.1f0bd2cf.js"
+  }
+]);
